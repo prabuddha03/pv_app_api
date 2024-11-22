2f8a7c6319086b0c8c04b7a848b1ccfcfadebe1e
Prabuddha Chowdhury <prabuddha.chowdhury@gmail.com>
2024-11-22 18:10:14 +0530

    config db and firebase

---
+++ config/db.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+require('dotenv').config(); // Ensure this is the very first line
+
+dotenv.config({ path: './.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(DB, {});
+    console.log('MongoDB connected');
+  } catch (error) {
+    console.error('MongoDB connection error:', error);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
