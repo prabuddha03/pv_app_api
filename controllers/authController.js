7ade667f8850c6625e58466bfb48acce86314f4a
Prabuddha Chowdhury <prabuddha.chowdhury@gmail.com>
2024-12-05 23:06:35 +0530

    protect middleware pushed

--- controllers/authController.js
+++ controllers/authController.js
@@ -74,3 +74,28 @@ const signToken = (id) =>
       },
     })
   })
+
+  exports.protect = catchAsync(async (req, res, next) => {
+    let token;
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.startsWith('Bearer')
+    ) {
+      token = req.headers.authorization.split(' ')[1];
+    }
+    if (!token) {
+      return next(
+        new AppError('You are not logged in! Please log in to access.', 401),
+      );
+    }
+  
+    const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+    const currentUser = await User.findById(decoded.id);
+    if (!currentUser) {
+      return next(new AppError('The user no longer exists.', 401));
+    }
+  
+    req.user = currentUser;
+    next();
+  });
+  
